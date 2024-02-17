90173d67216a2f2de0e823a55c2778e54688fca7
neetapatel <npfluter@gmail.com>
2024-02-16 19:49:18 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+function setup() {
+    // Set the date we're counting down to
+    var countDownDate = new Date("Mar 10, 2024 00:02:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="countdown"
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+    }, 1000);
+}
+
+$(function() {
+    setup();
+});
+
+// originally discovered this trick on mindy seu's website: https://mindyseu.com/ . . . then also found the code snippet on this stack overflow page: https://stackoverflow.com/questions/16354122/how-to-put-scrolling-text-in-title-tag
+(function titleScroller(text) {
+    document.title = text;
+    setTimeout(function () {
+        titleScroller(text.substr(1) + text.substr(0, 1));
+    }, 500);
+}("IS IT DAYLIGHT SAVING TIME YET? • "));
